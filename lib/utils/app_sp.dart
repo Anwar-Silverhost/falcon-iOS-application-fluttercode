ff61824dcd531f5e715569912fa77ab25d141402
Anwar-Silverhost <anwar@silverhost.in>
2024-09-03 17:40:13 +0530

    Login page api added code

--- lib/utils/app_sp.dart
+++ lib/utils/app_sp.dart
@@ -1,6 +1,16 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppSp {
+  Future<bool> setIsLogged(bool login) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setBool("isLogged", login);
+  }
+
+  Future<bool> getIsLogged() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool("isLogged") ?? false;
+  }
+
   Future<bool> setToken(String token) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.setString("token", token);
